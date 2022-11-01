a8e664ac1d4f786e60f901f35ba3c75dd2a4b62f
TianbinLiu <89219591+TianbinLiu@users.noreply.github.com>
2022-11-01 09:31:29 -0700

    Update videojava.js

--- assets/js/videojava.js
+++ assets/js/videojava.js
@@ -82,9 +82,7 @@ function lightbox_close() {
 
 
 function show(nr) {
-    document.getElementById("table1").style.display="none";
-    document.getElementById("table2").style.display="none";
-    document.getElementById("table3").style.display="none";
-    document.getElementById("table4").style.display="none";
-    document.getElementById("table"+nr).style.display="block";
+    document.getElementById("show1").style.display="none";
+    document.getElementById("show2").style.display="none";
+    document.getElementById("show"+nr).style.display="block";
 }
